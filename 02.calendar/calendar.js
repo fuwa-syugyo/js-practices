fadb83b57190a7205511434307d0e3d1667d79f4
fuwa <fuwafuwa41.glr@gmail.com>
2022-02-17 21:25:39 +0900

    該当の月の初日と最終日の取得を追加

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -10,3 +10,11 @@ let year = argv['y'] || now.year()
 console.log(month)
 console.log(year)
 
+let yesterday = new Date(year, month -1)
+let date = dayjs(yesterday)
+let start_of_month = date.startOf('month').format('D')
+let end_of_month = date.endOf('month').format('D')
+
+console.log(yesterday)
+console.log(start_of_month)
+console.log(end_of_month)
