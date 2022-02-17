5e03bb1748af6c170c59a8256897d2e98c899243
fuwa <fuwafuwa41.glr@gmail.com>
2022-02-17 14:53:51 +0900

    年と月未入力時は現在のを入れるようにした

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
+
 const dayjs = require('dayjs')
 let now = dayjs()
 
 let argv = require('minimist')(process.argv.slice(2))
-console.log(argv)
 
-let month = argv['m'] || now.month
-let year = argv['y'] || now.year
+let month = argv['m'] || now.month() + 1
+let year = argv['y'] || now.year()
 console.log(month)
 console.log(year)
+
