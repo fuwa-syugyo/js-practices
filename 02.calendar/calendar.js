ad29934a43a6a01c7597c542143454b14673bda2
fuwa <fuwafuwa41.glr@gmail.com>
2022-02-18 12:05:39 +0900

    とりあえず動くようにはなった

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -7,19 +7,11 @@ let argv = require('minimist')(process.argv.slice(2))
 
 let month = argv['m'] || now.month() + 1
 let year = argv['y'] || now.year()
-console.log(month)
-console.log(year)
 
-let yesterday = new Date(year, month -1)
-let date = dayjs(yesterday)
-let start_of_month = date.startOf('month').format('D')
+let first_day = new Date(year, month - 1, 2)
+let date = dayjs(first_day)
 let end_of_month = date.endOf('month').format('D')
 let space_count = date.startOf('month').format('d')
-
-console.log(yesterday)
-console.log(start_of_month)
-console.log(end_of_month)
-console.log(space_count)
 
 console.log('      ' + month + '月' + year + '     \n')
 console.log('日 月 火 水 木 金 土')
@@ -28,6 +20,13 @@ for (let cnt = 0; cnt < space_count; cnt++) {
   process.stdout.write('   ')
 }
 
-// for (let day = 1; day <= end_of_month; day++ ){
-//   process.stdout.write(day)
-// }
+for (let cnt = 0; cnt < parseInt(end_of_month); cnt++) {
+  if (cnt < 9) {
+    process.stdout.write(' ')
+  }
+  process.stdout.write(cnt + 1 + ' ')
+  let day_of_week = dayjs(first_day).add(cnt, 'day').format('d')
+  if (day_of_week === '0') {
+    console.log('')
+  }
+}
