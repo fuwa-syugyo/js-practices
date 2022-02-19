a6a4460bbe39bca93266cba10b251b5a2ea04fbd
fuwa <fuwafuwa41.glr@gmail.com>
2022-02-19 11:13:32 +0900

    %を消した

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,32 +1,33 @@
 #!/usr/bin/env node
 
 const dayjs = require('dayjs')
-let now = dayjs()
+const now = dayjs()
 
-let argv = require('minimist')(process.argv.slice(2))
+const argv = require('minimist')(process.argv.slice(2))
 
-let month = argv['m'] || now.month() + 1
-let year = argv['y'] || now.year()
+const month = argv.m || now.month() + 1
+const year = argv.y || now.year()
 
-let first_day = new Date(year, month - 1, 2)
-let date = dayjs(first_day)
-let end_of_month = date.endOf('month').format('D')
-let space_count = date.startOf('month').format('d')
+const firstDay = new Date(year, month - 1, 2)
+const date = dayjs(firstDay)
+const endOfMonth = date.endOf('month').format('D')
+const spaceCount = date.startOf('month').format('d')
 
-console.log('      ' + month + '月' + year + '     \n')
+console.log('      ' + month + '月 ' + year + '     ')
 console.log('日 月 火 水 木 金 土')
 
-for (let cnt = 0; cnt < space_count; cnt++) {
+for (let cnt = 0; cnt < spaceCount; cnt++) {
   process.stdout.write('   ')
 }
 
-for (let cnt = 0; cnt < parseInt(end_of_month); cnt++) {
+for (let cnt = 0; cnt < parseInt(endOfMonth); cnt++) {
   if (cnt < 9) {
     process.stdout.write(' ')
   }
   process.stdout.write(cnt + 1 + ' ')
-  let day_of_week = dayjs(first_day).add(cnt, 'day').format('d')
-  if (day_of_week === '0') {
+  const dayOfWeek = dayjs(firstDay).add(cnt, 'day').format('d')
+  if (dayOfWeek === '0') {
     console.log('')
   }
 }
+console.log('\n')
